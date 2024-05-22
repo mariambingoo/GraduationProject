c9a8a3c8bde378c799e0e9f6b1478adef180ddc1
Youssef Elharty <youssefashraf246@gmail.com>
2024-05-22 17:06:06 +0300

    Modified: .gitignore, Added: Project and model schema, Refactor: model directory

--- website/models/HomeModel.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    getData: () => {
-      return {
-        message: 'Welcome to my website!'
-      };
-    }
-  };
-  
---
+++ website/models/ProjectModel.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+const Model = require('./ModelModel')
+require('dotenv').config({ path: '../config/dev.env'})
+
+const ProjectScehma = new mongoose.Schema({
+  project_name:{
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description:{
+    type: String,
+    required: false,
+    trim: true,
+  },
+  date_created:{
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  // owner:{
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'User',
+  // }
+
+})
+
+ProjectScehma.virtual('models', {
+  ref: 'Model',
+  localField: '_id',
+  foreignField: 'project'
+})
+
+const Project = mongoose.model('Project', ProjectScehma, 'Projects')
+
+
+Project.prototype.saveData = async function () {
+    try {
+        await this.save()
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+module.exports = Project
+
+
+// // Project document
+// {
+//   _id: 123,
+//   name: 'Project 1',
+//   description: 'This is a project',
+//   // models: [id1, id2, id3]
+// }
+
+// // Model document
+// {
+//   _id: 456,
+//   name: 'Model 1',
+//   description: 'This is a model',
+//   project: 123
+// }
